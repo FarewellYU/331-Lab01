5da5d7690ad6fd15eacc19e237ddaff39b22213b
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-07-04 20:38:46 +0800

    before3.6

---
+++ main.js
@@ -0,0 +1,44 @@
+// const [createApp, ref] = Vue
+
+// createApp({
+//     setup(){
+//         const product = ref('Boots')
+//         const product1 = ref('socks')
+//         const image = ref('./assets/images/socks_green.jpg')
+//         return{
+//             product,
+//             product1,
+//             image
+//         }
+//     }
+// }).mount('#app')
+
+
+// const [ create, refe ] = Vue
+
+// create({
+//     setup(){
+//         const product = refe('Socks')
+//         return{
+//             product
+//         }
+//     }
+// }).mount('#app')
+
+const { createApp, ref } = Vue
+
+createApp({
+         setup() {
+     const product = ref('Boots');
+     const product1 = ref('socks');
+     const image = ref('./assets/images/socks_green.jpg');
+     const link = ref('http://www.camt.cmu.ac.th'); // Define the link URL here
+
+         return {
+    product,
+    product1,
+     image,
+     link
+     };
+    }
+    }).mount('#app');
