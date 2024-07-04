8454220e5d12e9c8ab9ea8ea565197c7fee0a469
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-07-04 21:29:02 +0800

    all 5

--- main.js
+++ main.js
@@ -34,8 +34,18 @@ createApp({
      const image = ref('./assets/images/socks_green.jpg');
      const link = ref('http://www.camt.cmu.ac.th'); // Define the link URL here
      const inStock = ref(false);
-     const inventory =ref(-9);
+     const inventory =ref(100);
      const onSale =ref(false);
+     const details =ref([
+        '50% cotton',
+        '30% wool',
+        '20% polyester'
+     ])
+     const variants = ref([
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ]);
+  
          return {
     product,
     product1,
@@ -43,7 +53,9 @@ createApp({
      link,
      inStock,
      inventory,
-     onSale
+     onSale,
+     details,
+     variants
 
      };
     }
